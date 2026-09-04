48fef280b0b512c6e2d4bc8b0d77dd492715030e
MrSaturnIdk <abrahm3314@gmail.com>
2026-09-04 20:05:45 +0000

    feat: make buttons and inputs change color

--- update.js
+++ update.js
@@ -12,12 +12,19 @@ const FONT = document.getElementById("font");
 const BG_COLOR = document.getElementById("bg-color");
 const TEXT_COLOR = document.getElementById("text-color");
 const TEXT = document.getElementById("text");
+const UPDATER = document.getElementById("update");
 
 function update() {
     MAIN_P.innerHTML = TEXT.value.trim();
     MAIN_P.style.fontSize = SIZE.value.trim();
     MAIN_P.style.fontFamily = FONT.value.trim();
     BODY.style.backgroundColor = BG_COLOR.value.trim();
+    TEXT_COLOR.style.backgroundColor = BG_COLOR.value.trim();
+    BG_COLOR.style.backgroundColor = BG_COLOR.value.trim();
+    TEXT.style.backgroundColor = BG_COLOR.value.trim();
+    SIZE.style.backgroundColor = BG_COLOR.value.trim();
+    FONT.style.backgroundColor = BG_COLOR.value.trim();
+    UPDATER.style.backgroundColor = BG_COLOR.value.trim();
     MAIN_P.style.color = TEXT_COLOR.value.trim();
 }
 
